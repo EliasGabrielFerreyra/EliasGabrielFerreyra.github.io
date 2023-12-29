759bb6c91e02bc49cff20012d66f1c4babaf00f4
EliasGabrielFerreyra <139504916+EliasGabrielFerreyra@users.noreply.github.com>
2023-12-29 10:39:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+
+  const synth = window.speechSynthesis;
+  
+  function voiceControl(string) {
+    let u = new SpeechSynthesisUtterance(string);
+    u.text = string;
+    u.lang = "en-aus";
+    u.volume = 1;
+    u.rate = 1;
+    u.pitch = 1;
+    synth.speak(u);
+  }
+
+  function sendMessage() {
+    const inputField = document.getElementById("input");
+    let input = inputField.value.trim();
+    input != "" && output(input);
+    inputField.value = "";
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const inputField = document.getElementById("input");
+    inputField.addEventListener("keydown", function (e) {
+      if (e.code === "Enter") {
+        let input = inputField.value.trim();
+        input != "" && output(input);
+        inputField.value = "";
+      }
+    });
+  });
+  function output(input) {
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+  
+    text = text
+      .replace(/[\W_]/g, " ")
+      .replace(/ a /g, " ")
+      .replace(/i feel /g, "")
+      .replace(/whats/g, "what is")
+      .replace(/please /g, "")
+      .replace(/ please/g, "")
+      .trim();
+  
+    // Llamada a la API
+    fetch("https://3363-186-148-66-139.ngrok-free.app", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        text: input,
+        hiddenContext: {}
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log('API Response:', result);
+      const responseText = JSON.stringify(result);
+      addChat(input, responseText);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      addChat(input, "Error fetching response"); // Manejamos el error mostrando un mensaje de error en la interfaz
+    });
+  }
+  
+  function addChat(input, product) {
+    const mainDiv = document.getElementById("message-section");
+    let userDiv = document.createElement("div");
+    userDiv.id = "user";
+    userDiv.classList.add("message");
+    userDiv.innerHTML = `<span id="user-response">${input}</span>`;
+    mainDiv.appendChild(userDiv);
+  
+    let botDiv = document.createElement("div");
+    botDiv.id = "bot";
+    botDiv.classList.add("message");
+    botDiv.innerHTML = `<span id="bot-response">${product}</span>`;
+    mainDiv.appendChild(botDiv);
+  
+    var scroll = document.getElementById("message-section");
+    scroll.scrollTop = scroll.scrollHeight;
+    voiceControl(product);
+  }
+  
+  function compare(triggerArray, replyArray, string) {
+    let item;
+    for (let x = 0; x < triggerArray.length; x++) {
+      for (let y = 0; y < replyArray.length; y++) {
+        if (triggerArray[x][y] == string) {
+          items = replyArray[x];
+          item = items[Math.floor(Math.random() * items.length)];
+        }
+      }
+    }
+    //containMessageCheck(string);
+    if (item) return item;
+    else return containMessageCheck(string);
+  }
+  
+  function containMessageCheck(string) {
+    let expectedReply = [
+      [
+        "Good Bye, dude",
+        "Bye, See you!",
+        "Dude, Bye. Take care of your health in this situation."
+      ],
+      ["Good Night, dude", "Have a sound sleep", "Sweet dreams"],
+      ["Have a pleasant evening!", "Good evening too", "Evening!"],
+      ["Good morning, Have a great day!", "Morning, dude!"],
+      ["Good Afternoon", "Noon, dude!", "Afternoon, dude!"]
+    ];
+    let expectedMessage = [
+      ["bye", "tc", "take care"],
+      ["night", "good night"],
+      ["evening", "good evening"],
+      ["morning", "good morning"],
+      ["noon"]
+    ];
+    let item;
+    for (let x = 0; x < expectedMessage.length; x++) {
+      if (expectedMessage[x].includes(string)) {
+        items = expectedReply[x];
+        item = items[Math.floor(Math.random() * items.length)];
+      }
+    }
+    return item;
+  }
